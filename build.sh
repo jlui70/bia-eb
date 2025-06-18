fc11a67d731603f1c9051fbf55598f1b8ecff1ff
jlui70 <luiz7030@gmail.com>
2025-06-18 16:36:53 -0300

    change 5

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
-aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin [SEU_REPOSITORIO]
+echo $versao
+aws ecr get-login-password --region us-east-1 --profile bia | docker login --username AWS --password-stdin 794038226274.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
-docker tag bia:latest [SEU_REPOSITORIO]/bia:$versao
-docker push [SEU_REPOSITORIO]/bia:$versao
+docker tag bia:latest 794038226274.dkr.ecr.us-east-1.amazonaws.com/bia:$versao
+docker push 794038226274.dkr.ecr.us-east-1.amazonaws.com/bia:latest$versao
 rm .env 2> /dev/null
 ./gerar-compose.sh
 rm bia-versao-*zip
